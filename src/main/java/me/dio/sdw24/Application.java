4b41057a42fdb98ff6fd1d8a81534c7ad7a92ca2
Jonhvmp <jonhpaz08@gmail.com>
2024-03-25 21:02:15 -0300

    plugs

--- src/main/java/me/dio/sdw24/Application.java
+++ src/main/java/me/dio/sdw24/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemosantanderDevWeek2024Application {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemosantanderDevWeek2024Application.class, args);
